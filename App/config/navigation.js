d2348450f10cef695619286407850ad9f841d944
Diego Silva <disilvap@microsoft.com>
2021-11-12 15:14:23 -0600

    drawer options

--- App/config/navigation.js
+++ App/config/navigation.js
@@ -59,9 +59,16 @@ const AppTabsScreen = () => (
 
 const AppDrawer = createDrawerNavigator();
 const AppDrawerScreen = () => (
-    <AppDrawer.Navigator>
-        <AppDrawer.Screen name="Tabs" component={AppTabsScreen} />
-        <AppDrawer.Screen name="Settings" component={Settings} />
+    <AppDrawer.Navigator 
+        drawerType="back"
+        // drawerPosition="right"
+        >
+        <AppDrawer.Screen name="Tabs" component={AppTabsScreen} options={{
+            drawerLabel: "Home"
+        }}/>
+        <AppDrawer.Screen name="Settings" component={Settings} options={{
+            gestureEnabled: false
+        }}/>
     </AppDrawer.Navigator>
 )
 

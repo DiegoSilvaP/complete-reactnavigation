736e3c65c5f72e3b7dd9abbde865116aa4979f4b
Diego Silva <disilvap@microsoft.com>
2021-11-12 15:25:34 -0600

    Navigating Between Different Navigators

--- App/screens/Settings.js
+++ App/screens/Settings.js
@@ -4,5 +4,6 @@ import { Button, SafeAreaView } from 'react-native';
 export default ({ navigation }) => (
   <SafeAreaView>
     <Button title="Toggle Drawer" onPress={() => navigation.toggleDrawer()} />
+    <Button title="To Actions" onPress={() => navigation.navigate("Tabs", {screen: "Actions"})} />
   </SafeAreaView>
 );
